d77b89a9df7450d3adfc102effb9cb34a7968a09
Prabhu <vprabhu1771@gmail.com>
2024-11-09 11:43:12 +0530

    DatabaseSeeder  User Import fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use\App\Models\User; 
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
